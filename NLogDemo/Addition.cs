2b15925ff246e4daca58a5fc00d64d802a9e5390
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-15 12:18:07 +0530

    [mohitkumar9884] adding/creating NLogDemo program

---
+++ NLogDemo/Addition.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    internal class Addition
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void SumOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int sum = 0;
+            if (firstNumber <= 0 || secondNumber <= 0)
+            {
+                logger.Warn("Inputs should be greater than zero");
+                logger.Error("Give proper +ve inputs");
+            }
+            else
+            {
+                sum = firstNumber + secondNumber;
+                logger.Info("Successfully added numbers and result is:{0}", sum);
+           
+            }
+        }
+    }
+}
---
+++ NLogDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 1st number");
+            int firstNumber=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter 2nd number");
+            int secondNumber=Convert.ToInt32(Console.ReadLine());
+            Addition.SumOfTwoNumbers(firstNumber,secondNumber);
+        }
+    }
+}
